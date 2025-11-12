b7a24531e6a1b76ab20e4029c7d0c2cd9e5bd0a0
hemant parte <166102426+hemant1022@users.noreply.github.com>
2025-11-12 23:48:54 +0530

    Add project/models.py

---
+++ project/models.py
@@ -0,0 +1,43 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    orders = db.relationship('Order', backref='customer', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    def __repr__(self):
+        return f"Product('{self.name}', '{self.price}')"
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_ordered = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    items = db.relationship('OrderItem', backref='order', lazy=True, cascade="all, delete-orphan")
+    total = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Order('Order #{self.id}', 'Date: {self.date_ordered}')"
+
+class OrderItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+    price = db.Column(db.Float, nullable=False)
+
+    product = db.relationship('Product')
+
+    def __repr__(self):
+        return f"OrderItem('Product ID: {self.product_id}', 'Quantity: {self.quantity}')"
